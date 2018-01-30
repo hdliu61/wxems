4202c3a9755b9d0032e0b699df03484208473585
LiuYu <270094542@qq.com>
2018-01-31 00:55:54 +0800

    项目初始化部署

---
+++ src/main/java/com/hdliu/wxems/WxemsApplication.java
@@ -0,0 +1,12 @@
+package com.hdliu.wxems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WxemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WxemsApplication.class, args);
+	}
+}
